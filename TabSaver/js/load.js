c9d634b5996fc1a1392affe2b6f0c4beaaa2fa43
alanmerritt <merrittalan1@gmail.com>
2016-10-10 22:18:56 -0600

    Uploaded new load/save files.
    Implemented save functionality. Have yet to get loading to work.

---
+++ TabSaver/js/load.js
@@ -0,0 +1,10 @@
+
+
+
+//TODO: load from localstorage on document
+//and save.
+document.addEventListener("onload", function(e) {
+	
+	document.getElementById("debug").innerHTML = "Page loaded";
+	
+});
--- TabSaver/js/save.js
+++ TabSaver/js/save.js
@@ -1,1 +1,20 @@
 
+
+var btn_save = document.getElementById("save");
+var container = document.getElementById("container");
+
+
+btn_save.addEventListener("click", function(e) {
+	
+	for(var i = 0; i < container.childElementCount; i++) {
+		
+		var key = i.toString();
+		var value = container.childNodes[i].childNodes[0].innerHTML;
+		
+		localStorage.setItem(key, value);
+		
+		//document.getElementById("debug").innerHTML = "key: " + key + " - value: " + container.childNodes[i].childNodes[0].innerHTML;
+		
+	}
+	
+});
